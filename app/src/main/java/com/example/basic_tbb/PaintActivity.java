55ef5d33a9019d18dd85e04a550f049e61baad54
Ethan Bao <ebao@zagmail.gonzaga.edu>
2021-12-09 22:57:47 -0800

    fixed up the paint activity and implemented ability to save to photo gallery

--- app/src/main/java/com/example/basic_tbb/PaintActivity.java
+++ app/src/main/java/com/example/basic_tbb/PaintActivity.java
@@ -1,27 +1,209 @@
 package com.example.basic_tbb;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
-import android.graphics.Paint;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.media.MediaScannerConnection;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
-import android.widget.Spinner;
+import android.widget.RelativeLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import yuku.ambilwarna.AmbilWarnaDialog;
 
 public class PaintActivity extends AppCompatActivity {
-    private Button returnButton;
-    private Spinner colorSpinner; //placeholder for color wheel to be implemented
+
+    private CanvasExporter canvasExporter;
+    private PaintView paintView;
+    private int defaultColor;
+    private int STORAGE_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Button button;
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_paint);
 
-        returnButton = findViewById(R.id.return_button);
-        colorSpinner = findViewById(R.id.color_spinner);
+        canvasExporter = new CanvasExporter();
+        paintView = findViewById(R.id.paintView);
+        button = findViewById(R.id.change_color_button);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        SeekBar seekBar = findViewById(R.id.seekBar);
+        final TextView textView = findViewById(R.id.current_pen_size);
 
-        PaintView paintView = new PaintView(this);
-        setContentView(paintView);
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
 
+        paintView.initialize(displayMetrics);
 
+        textView.setText("Pen size: " + seekBar.getProgress());
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openColorPicker();
+            }
+        });
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                paintView.setStrokeWidth(seekBar.getProgress());
+                textView.setText("Pen size: " + seekBar.getProgress());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.main_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void requestStoragePermission () {
+        String permission = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+        // request the permission to write to storage
+        ActivityCompat.requestPermissions(this, new String[]{permission},
+                CanvasExporter.PERMISSION_WRITE_EXTERNAL_STORAGE);
+    }
+
+    private void checkForPermissions()
+    {
+        int permission = ContextCompat.checkSelfPermission(
+                this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        if (permission == PackageManager.PERMISSION_DENIED)
+        {
+            // if the user has not granted permission
+            boolean shouldShowRationale = ActivityCompat.shouldShowRequestPermissionRationale(
+                    this, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+            if (shouldShowRationale)
+            {
+                // display a rationale for saving (why the permission is needed)
+                StorageRationaleDialog dialog = new StorageRationaleDialog(PaintActivity.this);
+                dialog.setOnStorageRationaleOptionSelectedListener(new StorageRationaleDialog.StorageRationaleOptionSelectedListener()
+                {
+                    /**
+                     * Callback method which handles whether a user accepted the permission or not.
+                     * @param allow - whether the permission is accepted or not.
+                     */
+                    @Override
+                    public void onStorageRationaleOptionSelected(boolean allow)
+                    {
+                        // if the user accepts the storage permission in the dialog, request it officially
+                        if (allow)
+                            requestStoragePermission();
+                    }
+                });
+                dialog.show();
+            } else
+            {
+                // request the permission to write to storage
+                requestStoragePermission();
+            }
+        } else
+        {
+            // save/share the image as permission already granted
+            exportImage();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Access granted", Toast.LENGTH_SHORT).show();
+                exportImage();
+            } else {
+                Toast.makeText(this, "Access denied", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.clear_button:
+                paintView.clear();
+                return true;
+            case R.id.undo_button:
+                paintView.undo();
+                return true;
+            case R.id.redo_button:
+                paintView.redo();
+                return true;
+            case R.id.save_button:
+                canvasExporter.setExportType(CanvasExporter.FLAG_SAVE);
+                checkForPermissions();
+                return true;
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openColorPicker() {
+        AmbilWarnaDialog ambilWarnaDialog = new AmbilWarnaDialog(this, defaultColor, new AmbilWarnaDialog.OnAmbilWarnaListener() {
+            @Override
+            public void onCancel(AmbilWarnaDialog dialog) {
+                Toast.makeText(PaintActivity.this, "Unavailable", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onOk(AmbilWarnaDialog dialog, int color) {
+                defaultColor = color;
+                paintView.setColor(color);
+            }
+        });
+        ambilWarnaDialog.show();
+    }
+
+    private void exportImage ()
+    {
+        if (canvasExporter.getExportType() == CanvasExporter.FLAG_SAVE)
+        {
+            // if the user is wanting to save, attempt and return its filename
+            String fileName = canvasExporter.saveImage(paintView.getmBitmap());
+
+            if (fileName != null)
+            {
+                // refresh the gallery to show the new image if it exists
+                MediaScannerConnection.scanFile(
+                        PaintActivity.this, new String[]{fileName}, null, null);
+                Toast.makeText(PaintActivity.this, "The image was saved successfully.", Toast.LENGTH_SHORT).show();
+            } else
+            {
+                Toast.makeText(PaintActivity.this, "There was an error saving the image.", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/basic_tbb/PaintView.java
+++ app/src/main/java/com/example/basic_tbb/PaintView.java
@@ -1,59 +1,179 @@
 package com.example.basic_tbb;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.os.Environment;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class PaintView extends View {
+    public static int BRUSH_SIZE = 10;
+    public static final int DEFAULT_COLOR = Color.RED;
+    public static final int DEFAULT_BG_COLOR = Color.WHITE;
+    private static final float TOUCH_TOLERANCE = 4;
 
-    public LayoutParams params;
-    private Path path = new Path();
-    private Paint brush = new Paint();
+    public float mX, mY;
+    private Path mPath;
+    private Paint mPaint;
+    private int currentColor;
+    private int backgroundColor = DEFAULT_BG_COLOR;
+    private int strokeWidth;
+    private Bitmap mBitmap;
+    private Canvas mCanvas;
+    private Paint mBitmapPaint = new Paint(Paint.DITHER_FLAG);
 
+    private ArrayList<Draw> paths = new ArrayList<>();
+    private ArrayList<Draw> undo = new ArrayList<>();
 
     public PaintView(Context context) {
-        super(context);
+        super(context, null);
+    }
 
-        brush.setAntiAlias(true);
-        brush.setColor(Color.GREEN);
-        brush.setStyle(Paint.Style.STROKE);
-        brush.setStrokeJoin(Paint.Join.ROUND);
-        brush.setStrokeWidth(8f);
+    public PaintView(Context context, AttributeSet attrs) {
+        super(context, attrs);
 
-        params = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        mPaint = new Paint();
+        mPaint.setAntiAlias(true);
+        mPaint.setDither(true);
+        mPaint.setColor(DEFAULT_COLOR);
+        mPaint.setStyle(Paint.Style.STROKE);
+        mPaint.setStrokeJoin(Paint.Join.ROUND);
+        mPaint.setStrokeCap(Paint.Cap.ROUND);
+        mPaint.setXfermode(null);
+        mPaint.setAlpha(0xff);
+    }
 
+    public Bitmap getmBitmap() {
+        return mBitmap;
+    }
+
+    public void setmBitmap(Bitmap mBitmap) {
+        this.mBitmap = mBitmap;
+    }
+
+    public void initialize (DisplayMetrics displayMetrics) {
+        int height = displayMetrics.heightPixels;
+        int width = displayMetrics.widthPixels;
+
+        mBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        mCanvas = new Canvas(mBitmap);
+
+        currentColor = DEFAULT_COLOR;
+        strokeWidth = BRUSH_SIZE;
     }
 
     @Override
-    public boolean onTouchEvent(MotionEvent event)
-    {
-        float pointX = event.getX();
-        float pointY = event.getY();
+    protected void onDraw(Canvas canvas) {
+        canvas.save();
+        mCanvas.drawColor(backgroundColor);
 
-        switch(event.getAction())
-        {
-            case MotionEvent.ACTION_DOWN:
-                path.moveTo(pointX,pointY);
-                return true;
-            case MotionEvent.ACTION_MOVE:
-                path.lineTo(pointX,pointY);
-                break;
-            default:
-                return false;
+        for (Draw draw : paths) {
+            mPaint.setColor(draw.color);
+            mPaint.setStrokeWidth(strokeWidth);
+            mPaint.setMaskFilter(null);
+
+            mCanvas.drawPath(draw.path, mPaint);
         }
-        postInvalidate();
-        return false;
+
+        canvas.drawBitmap(mBitmap, 0, 0, mBitmapPaint);
+        canvas.restore();
     }
 
+    private void touchStart (float x, float y) {
+        mPath = new Path();
+
+        Draw draw = new Draw(currentColor, strokeWidth, mPath);
+        paths.add(draw);
+
+        mPath.reset();
+        mPath.moveTo(x, y);
+
+        mX = x;
+        mY = y;
+    }
+
+    private void touchMove (float x, float y) {
+        float dx = Math.abs(x - mX);
+        float dy = Math.abs(y - mY);
+
+        if(dx >= TOUCH_TOLERANCE || dy >= TOUCH_TOLERANCE) {
+            mPath.quadTo(mX, mY, (x + mX)/2, (y + mY)/2);
+
+            mX = x;
+            mY = y;
+        }
+    }
+
+    private void touchUp() {
+        mPath.lineTo(mX, mY);
+    }
 
     @Override
-    protected void onDraw(Canvas canvas)
-    {
-        canvas.drawPath(path, brush);
+    public boolean onTouchEvent(MotionEvent event) {
+        float x = event.getX();
+        float y = event.getY();
+
+        switch(event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                touchStart(x, y);
+                invalidate();
+                break;
+            case MotionEvent.ACTION_UP:
+                touchUp();
+                invalidate();
+                break;
+            case MotionEvent.ACTION_MOVE:
+                touchMove(x, y);
+                invalidate();
+                break;
+        }
+        return true;
+    }
+
+    public void clear() {
+        backgroundColor = DEFAULT_BG_COLOR;
+        paths.clear();
+        invalidate();
+    }
+
+    public void undo() {
+        if(paths.size() > 0) {
+            undo.add(paths.remove(paths.size() - 1));
+            invalidate();
+        }
+        else {
+            Toast.makeText(getContext(), "Nothing to undo", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void redo() {
+        if(undo.size() > 0) {
+            paths.add(undo.remove(undo.size() - 1));
+            invalidate();
+        }
+        else {
+            Toast.makeText(getContext(), "Nothing to redo", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public void setStrokeWidth(int width) {
+        strokeWidth = width;
+    }
+
+    public void setColor(int color) {
+        currentColor = color;
     }
 }
